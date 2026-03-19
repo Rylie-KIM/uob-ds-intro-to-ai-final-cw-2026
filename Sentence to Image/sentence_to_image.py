da2a39ec30b9bcb097c4a169ff977939da60e120
jn25200 <ferg.watson23@gmail.com>
2026-03-19 13:56:49 +0000

    updated draw method to take text as inputs and configure shapes

--- Sentence to Image/sentence_to_image.py
+++ Sentence to Image/sentence_to_image.py
@@ -1,5 +1,5 @@
 import turtle
-
+import random
 class Shapes(turtle.Turtle):
     """ 
     Pixel range is determined by screen = turtle.Screen() , screen.setup(width=500, height=500)
@@ -26,7 +26,14 @@ class Shapes(turtle.Turtle):
         self.size = {'big':100,
                      'medium':70,
                      'small':50}
-        
+        """
+        self.relations = {
+            'above':self.draw_above,
+            'below':self.draw_bellow,
+            'left of':self.draw_left,
+            'right of': self.draw_right
+        }
+        """
 
     def move(self, go_to):
         self.penup()
@@ -65,15 +72,41 @@ class Shapes(turtle.Turtle):
         self.end_fill()
         return self
     
-    def draw(self, object, go_to, colour):
+    def draw1(self, object, colour):
+
+        go_to = random.randint(-250, 250), random.randint(-250, 250)
         self.hideturtle()
         shape = self.object[object]
         shape(go_to, colour)
         return self
-
+    
+    def draw(self,s1, c1, o1, s2, c2, o2, rel):
+        self.hideturtle()
+        # relation = self.relations[rel]
+        x, y = random.randint(-180, 180), random.randint(-180, 180)
+        shape = self.object[o1]
+        shape((x,y), c1)
+        x2, y2 = self.coordinates(x,y,rel)
+        shape2 = self.object[o2]
+        shape2((x2,y2), c2)
+        return self
+    
+    def coordinates(self,x, y, relation):
+        if relation == 'above':
+            x2, y2 = x + random.randint(-30,30), y - random.randint(50,150)
+            return x2,y2
+        elif relation =='below':
+            x2, y2 = x + random.randint(-30,30), y + random.randint(50,150)
+            return x2, y2
+        elif relation == 'right of':
+            x2, y2 = x + random.randint(50,150), y + random.randint(-30,30)
+            return x2, y2
+        elif relation == 'left of':
+            x2, y2 = x - random.randint(50,150), y + random.randint(-30,30)
+            return x2,y2
 
 screen = turtle.Screen()
 screen.setup(width=500, height=500)
 s = Shapes()
-s.draw('square', (100,100), 'red').draw('triangle', (-80, -50), 'yellow')
+s.draw(s1='big', c1='red', o1='circle', s2='small', c2='blue', o2='square',rel='above')
 turtle.done()
