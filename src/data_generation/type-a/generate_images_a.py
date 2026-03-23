9be20ded1b9785ac0e6ad07bad5f0ba6171757eb
jn25200 <ferg.watson23@gmail.com>
2026-03-23 14:43:18 +0000

    Debugged Shapes class
    debugged class by ensuring that images could no longer generate on top of themselves. Added get_width_height and get_dims methods to enable the accurate centering of shapes around coordinates. Updated coodinates method to include x and y dimensions for space between objects

--- src/data_generation/type-a/generate_images_a.py
+++ src/data_generation/type-a/generate_images_a.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import math
 
 class Shapes(turtle.Turtle):
     """ 
@@ -26,13 +27,14 @@ class Shapes(turtle.Turtle):
         self.goto(go_to)
         self.pendown()
         return self
-
+    
+    ############
+    ############
+    # The shapes ######
     def square(self, go_to:tuple[int,int], colour:str, size:int):
         """
         square((50,75), 'red', 100)
         """
-        
-        size = self.size[size]
         # This makes the initial go_to point the center.
         go_to = go_to[0] - (size/2), go_to[1] - (size/2)
         self.color(colour, colour)
@@ -45,26 +47,25 @@ class Shapes(turtle.Turtle):
         return self
 
     def my_circle(self, go_to:tuple[int,int], colour:str, size:int):
-    
         """
         my_circle((50,75), 'red', 100)
         """
-        size = self.size[size]
-        go_to = go_to[0], go_to[1] - (size/2)
+        go_to = go_to[0] - (size/2), go_to[1] - (size/2)
         self.color(colour, colour)
         self.move(go_to)
         self.begin_fill()
         self.circle(size/2)
         self.end_fill()
         return self
-    
+
     def triangle(self, go_to:tuple[int,int], colour:str, size:int):
         """
         triangle((50,75), 'red', 100)
+        Have to use Herons formula to calculate the height of a triangle area = (1/2)(base)(height)
+        rearranges to..... 
         """
-
-        size = self.size[size]
-        go_to = go_to[0] - (size/2), go_to[1] - (size/2)
+        width, height = self.get_triangle_dims(size)
+        go_to = go_to[0] - (width/2), go_to[1] - (height/2)
         self.color(colour, colour)
         self.move(go_to)
         self.begin_fill()
@@ -77,10 +78,9 @@ class Shapes(turtle.Turtle):
     def diamond(self, go_to:tuple[int,int], colour:str, size:int):
         """ 
         diagmond((50,75), 'black', 120))
-        
         """
-        size = self.size[size]
-        go_to = go_to[0] - (size/2), go_to[1] - (size/2)
+        width, height = self.get_dims('diamond', size)
+        go_to = go_to[0] - (width/2), go_to[1] - (height/2)
         self.color(colour, colour)
         self.move(go_to)
         self.begin_fill()
@@ -95,13 +95,13 @@ class Shapes(turtle.Turtle):
         """
         hexagon((50,75), 'orange', 60))
         """
-        size = self.size[size]
-        go_to = go_to[0] - (size/2), go_to[1] - (size/2)
+        width, height = self.get_dims('hexagon', size)
+        go_to = go_to[0] - (width/2), go_to[1] - (height/2)
         self.color(colour, colour)
         self.move(go_to)
         self.begin_fill()
         for i in range(6):
-            self.forward(size/2)
+            self.forward(size)
             self.left(360/6)
         self.end_fill()
         return self
@@ -110,8 +110,8 @@ class Shapes(turtle.Turtle):
         """
         octagon((50,75), 'blue', 50))
         """
-        size = self.size[size] // 2.5
-        go_to = go_to[0] - (size/2), go_to[1] - (size/2)
+        width, height = self.get_dims('octagon', size)
+        go_to = go_to[0] - (width/2), go_to[1] - (height/2)
         self.color(colour, colour)
         self.move(go_to)
         self.begin_fill()
@@ -120,14 +120,75 @@ class Shapes(turtle.Turtle):
             self.left(45)
         self.end_fill()
         return self
-        
+
+
     def draw1(self, object, colour, size):
         go_to = random.randint(-250, 250), random.randint(-250, 250)
         self.hideturtle()
         shape = self.object[object]
         shape(go_to, colour, size)
         return self
-    
+
+    def get_width_height(self, shape:str, size:int):
+        if shape == 'square' or shape == 'circle':
+            size = self.size[size]
+            return size,size
+        elif shape == 'traingle':
+            width, height = self.get_dims(shape, size)
+            return width, height
+        elif shape == 'diamond':
+            width, height = self.get_dims(shape, size)
+            return width, height
+        elif shape == 'hexagon':
+            width,height = self.get_dims(shape, size)
+            return width, height
+        elif shape == 'octagon':
+            width,height = self.get_dims(shape, size)
+            return width, height
+
+    def get_dims(self, shape, size):
+        if shape =='square' or shape =='circle' or shape =='diamond':
+            return size, size
+        elif shape == 'triangle':
+            s = (size*3)/2
+            A = math.sqrt((s * (s - size)**3))
+            height =  A / ((1/2)*size)
+            width = size
+            return width, height
+        elif shape =='hexagon':
+            width = 2 * size
+            a = (math.sqrt(3)/2) * size
+            height = 2*a
+            return width, height
+        elif shape == 'octagon':
+            width = size * (math.sqrt((4 + 2*(math.sqrt(2)))))
+            height = width
+            return width, height
+
+    def coordinates(self,x:int, y:int, relation:str, s1:int, s2:int,o1:str, o2:str):
+        """
+        The coordinates method begins by calculating the available space ie how much space each of the shapes takes up, defined by the size parameter
+        This is done in order to make the x or y coordinates generate relative to the size of each shape individually as to prevent overlapping.
+
+        """
+        width1, height1 = self.get_width_height(o1, s1)
+        width2, height2 = self.get_width_height(o2, s2)
+        x_space = int((width1/2) + (width2/2))
+        y_space = int((height1/2) + (height2))
+        if relation == 'above':
+            # i shoudl definitely add a screen width param to generate larger variance in shape distances
+            x2, y2 = x + random.randint(-30,30), y - random.randint(y_space,y_space + 100)
+            return x2,y2
+        elif relation =='below':
+            x2, y2 = x + random.randint(-30,30), y + random.randint(y_space,y_space + 100)
+            return x2, y2
+        elif relation == 'right of':
+            x2, y2 = x - random.randint(x_space,x_space + 100), y + random.randint(-30,30)
+            return x2, y2
+        elif relation == 'left of':
+            x2, y2 = x + random.randint(x_space,x_space + 100), y + random.randint(-30,30)
+            return x2,y2
+        
     def draw(self,s1:int, c1:str, o1:str, s2:int, c2:str, o2:str, rel:str):
         """
         The .draw method takes a mix of str and int inputs to corresponding to size, colour and object_type for one of two objects
@@ -136,32 +197,21 @@ class Shapes(turtle.Turtle):
         ie where relation is 'above' the second shapes coordinates are generated in the area bellow the shape.
         
         """
+        # Converts size from str ie 'big' to int value
+        s1 = self.size[s1]
+        s2 = self.size[s2]
         self.hideturtle()
-        x, y = random.randint(-180, 180), random.randint(-180, 180)
+        go_to = random.randint(-180, 180), random.randint(-180, 180)
         shape = self.object[o1]
-        shape((x,y), c1, s1)
-        x2, y2 = self.coordinates(x,y,rel, s1, s2)
+        shape(go_to, c1, s1)
+        go_to2 = self.coordinates(go_to[0],go_to[1],rel, s1, s2, o1, o2)
         shape2 = self.object[o2]
-        shape2((x2,y2), c2, s2)
+        shape2(go_to2, c2, s2)
         return self
-    
-    def coordinates(self,x:int, y:int, relation:str, s1:int, s2:int):
-        """
-        The coordinates method begins by calculating the available space ie how much space each of the shapes takes up, defined by the size parameter
-        This is done in order to make the x or y coordinates generate relative to the size of each shape individually as to prevent overlapping.
 
-        """
-        space = (self.size[s1]//2) + (self.size[s2]//2)
-        if relation == 'above':
-            x2, y2 = x + random.randint(-30,30), y - random.randint(space,space + 100)
-            return x2,y2
-        elif relation =='below':
-            x2, y2 = x + random.randint(-30,30), y + random.randint(space,space + 100)
-            return x2, y2
-        elif relation == 'right of':
-            x2, y2 = x - random.randint(space,space + 100), y + random.randint(-30,30)
-            return x2, y2
-        elif relation == 'left of':
-            x2, y2 = x + random.randint(space,space + 100), y + random.randint(-30,30)
-            return x2,y2
-
+screen = turtle.Screen()
+screen.setup(width=500, height=500)
+s = Shapes()
+# s.hexagon((50,75), 'orange', 'big')
+s.draw(s1='small', c1='red', o1='octagon', s2='small', c2='black', o2='hexagon', rel='above')
+turtle.done()
