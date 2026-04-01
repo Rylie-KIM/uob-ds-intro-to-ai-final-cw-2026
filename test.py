80ea2c7cbb817b35e756d4962bb25fca097bd742
jn25200 <ferg.watson23@gmail.com>
2026-04-01 12:48:25 +0100

    added cnn model file and began implementing cnn model

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print(max(100, 250))
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+path = r'C:\Users\fergu\Documents\GitHub\uob-ds-intro-to-ai-final-cw-2026\src\data\type-a\type-a-dataset\type-a-dataset-png\1.png'
+img = Image.open(path).convert('RGB')
+# img.show()
+np_img = np.array(img)
+print(np_img.shape)
+plt.imshow(np_img)
+plt.axis('off')
+plt.show()
