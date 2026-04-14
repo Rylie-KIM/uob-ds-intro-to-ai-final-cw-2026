8bf3f2cff7cfb3f8eabd0545326fd30beaa7c391
jn25200 <ferg.watson23@gmail.com>
2026-04-14 15:51:04 +0100

    tested training time for googlenet model

--- src/models/googleNet.py
+++ src/models/googleNet.py
@@ -5,7 +5,7 @@ from torchvision.models import GoogLeNet_Weights
 
 class GoogleNet(nn.Module):
 
-    def __init__(self, output_dim):
+    def __init__(self, output_dims):
         super().__init__()
         # Inputs : [batch_size, 3,224,224]
         self.encoder = models.googlenet(weights=GoogLeNet_Weights.DEFAULT)
@@ -17,28 +17,10 @@ class GoogleNet(nn.Module):
         self.custom_fc = nn.Sequential(
             nn.Linear(1024, 512),
             nn.ReLU(),
-            nn.Linear(512, output_dim)                
+            nn.Linear(512, output_dims)                
         )
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.custom_fc(x)
-        print(f'Output Shape:\n{x.shape}')
-        print(f'Output:\n{x}')
         return x
-
-
-
-from PIL import Image
-path = r'C:\Users\fergu\Documents\GitHub\uob-ds-intro-to-ai-final-cw-2026\src\data\images\type-a\a_1.png'
-img = Image.open(path).convert('RGB')
-
-transforms = GoogLeNet_Weights.DEFAULT.transforms()
-
-img = transforms(img)
-
-print(img.shape)
-
-cnn = GoogleNet(output_dim = 512)
-print(f'Input Shape: {img.unsqueeze(0).shape}')
-cnn(img.unsqueeze(0))
